0f138aea566620dddbcaa566292403f658659d93
허은주 <rnlduadl135@coinvest.kr>
2020-02-18 15:51:00 +0900

    커밋

--- js/common.js
+++ js/common.js
@@ -2,17 +2,18 @@ $(document).ready(function () {
     // header scrolled
     var header = $('.header');
     setHeader();
-    $(window).on('resize', function () {
+    $(window).on('resize', function() {
         setHeader();
     });
-    $(document).on('scroll', function () {
+    $(document).on('scroll', function() {
         setHeader();
+        console.log(setHeader());
     });
-
     function setHeader() {
-        if ($(window).scrollTop() > 0) {
+        if($(window).scrollTop() > 0) {
             header.addClass('scrolled');
-        } else {
+        }
+        else {
             header.removeClass('scrolled');
         }
     }
@@ -31,17 +32,35 @@ $(document).ready(function () {
         $(this).parent('.header').toggleClass('height-auto');
     });
 
+    $(".full-nav-list li .nav-item").click(function (e) {
+        e.preventDefault();
+        var goSection = $(this).attr("href");
+
+        var goSectionTop = 0;
+        if ($(goSection).offset()) {
+            goSectionTop = $(goSection).offset().top
+        }
+
+        $("html, body").animate({scrollTop: goSectionTop - 70}, 500, "easeOutQuad");
+        $("#mobileBtn").removeClass('active');
+        $('#full-nav').removeClass('open');
+        $("#mobileBtn").parent('.header').toggleClass('height-auto');
+        return true;
+    });
+
     //scroll top
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 0 ) {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
             $('#goTop:hidden').stop(true, true).fadeIn();
         } else {
             $('#goTop').stop(true, true).fadeOut();
         }
     });
-    $(function(){$("#goTop").click(function(){
-        $("html,body").animate({scrollTop:$("body").offset().top},"1000");
-        return false})
+    $(function () {
+        $("#goTop").click(function () {
+            $("html,body").animate({scrollTop: $("body").offset().top}, "1000");
+            return false
+        })
     });
 
     // animation
